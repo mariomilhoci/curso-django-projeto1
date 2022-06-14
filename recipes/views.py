e425f01f20a475f24752777815f18d98fef6dba0
Mário César Milhoci <mario.milhoci@outlook.pt>
2022-06-14 14:05:54 -0300

    templates + html

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Mário César Milhoci'})
-
-
-def sobre(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Mário César Milhoci'})
