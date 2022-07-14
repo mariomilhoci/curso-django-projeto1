6e8dce613be5184ae8e03f61c6cb06fa4b439559
Mário César Milhoci <mario.milhoci@outlook.pt>
2022-07-13 21:38:38 -0300

    Django Forms / Testes funcionais

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -51,9 +51,7 @@ class RecipeHomeViewsTest(RecipeTestBase):
         )
 
     def test_recipe_home_is_paginated(self):
-        for i in range(8):
-            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
-            self.make_recipe(**kwargs)
+        self.make_recipe_in_batch(qtd=8)
 
         with patch('recipes.views.PER_PAGE', new=3):
             response = self.client.get(reverse('recipes:home'))
@@ -64,3 +62,18 @@ class RecipeHomeViewsTest(RecipeTestBase):
             self.assertEqual(len(paginator.get_page(1)), 3)
             self.assertEqual(len(paginator.get_page(2)), 3)
             self.assertEqual(len(paginator.get_page(3)), 2)
+
+    def test_invalid_page_query_uses_page_one(self):
+        self.make_recipe_in_batch(qtd=8)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home') + '?page=1A')
+            self.assertEqual(
+                response.context['recipes'].number,
+                1
+            )
+            response = self.client.get(reverse('recipes:home') + '?page=3')
+            self.assertEqual(
+                response.context['recipes'].number,
+                3
+            )
